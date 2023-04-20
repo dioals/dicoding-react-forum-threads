c45ea3e39a8e2964c2c9e4f835f0cddc8be18790
Dio Als <alsabahdio@gmail.com>
2023-04-20 16:28:18 +0700

    make test pass

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,13 +3,13 @@ import { useState } from 'react';
 function useInput(defaultValue = '') {
   const [value, setValue] = useState(defaultValue);
 
-  // function handleValueChange({ target }) {
-  //   try {
-  //     setValue(target.value);
-  //   } catch (error) {
-  //     setValue('');
-  //   }
-  // }
+  function handleValueChange({ target }) {
+    try {
+      setValue(target.value);
+    } catch (error) {
+      setValue('');
+    }
+  }
 
   return [value, handleValueChange, setValue];
 }
